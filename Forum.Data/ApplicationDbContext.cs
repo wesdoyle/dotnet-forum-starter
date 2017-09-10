35845eff01044d1e15ad58bee2edb1735d48cbc9
Wes D <wesdoyle08@gmail.com>
2017-09-09 19:05:17 -0500

    Move models and dbcontext to Data project

--- Forum.Data/ApplicationDbContext.cs
+++ Forum.Data/ApplicationDbContext.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using forum_app_demo.Models;
 
 namespace forum_app_demo.Data
 {
--- Forum.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Forum.Service
-{
-    public class Class1
-    {
-    }
-}
