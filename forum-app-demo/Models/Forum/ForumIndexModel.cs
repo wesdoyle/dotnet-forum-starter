b167232993d5c19b32c3fdd678a6c20296e5141a
Wes D <wesdoyle08@gmail.com>
2017-09-09 22:55:47 -0500

    Add forum view models

---
+++ forum-app-demo/Models/Forum/ForumIndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Forum.Web.Models.Forum
+{
+    public class ForumIndexModel
+    {
+        public int NumberOfForums { get; set; }
+        public IEnumerable<ForumListingModel> ForumList { get; set; }
+    }
+}
---
+++ forum-app-demo/Models/Forum/ForumListingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Forum.Web.Models.Forum
+{
+    public class ForumListingModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPosts { get; set; }
+        public int NumberOfUsers { get; set; }
+        public DateTime LatestActivity { get; set; }
+    }
+}
