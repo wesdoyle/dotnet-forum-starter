772f4cd9c2773f701598b0c2e290a736db00b9a9
Wes D <wesdoyle08@gmail.com>
2017-09-09 22:52:47 -0500

    Create delete forum view model

---
+++ forum-app-demo/Models/Forum/DeleteForumModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Web.Models.Forum
+{
+    public class DeleteForumModel
+    {
+        public int ForumId { get; set; }
+        public string ForumName { get; set; }
+        public int UserId { get; set; }
+    }
+}
