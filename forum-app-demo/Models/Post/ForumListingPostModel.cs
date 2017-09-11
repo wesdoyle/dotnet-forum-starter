40cfc540354574d8cb62ff48ea24d36a9ff6701e
Wes D <wesdoyle08@gmail.com>
2017-09-11 10:56:20 -0500

    Work on topic

--- forum-app-demo/Models/Post/ForumListingPostModel.cs
+++ forum-app-demo/Models/Post/ForumListingPostModel.cs
@@ -4,6 +4,7 @@ namespace Forum.Web.Models.Post
 {
     public class ForumListingPostModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string DatePosted { get; set; }
