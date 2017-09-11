b6fea7d76bbf09f09436bfe233d2db2ad7b78090
Wes D <wesdoyle08@gmail.com>
2017-09-11 10:38:50 -0500

    Forum index update

--- forum-app-demo/Models/Forum/ForumListingModel.cs
+++ forum-app-demo/Models/Forum/ForumListingModel.cs
@@ -1,11 +1,11 @@
 ﻿using Forum.Web.Models.Post;
-using System;
 using System.Collections.Generic;
 
 namespace Forum.Web.Models.Forum
 {
     public class ForumListingModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int NumberOfPosts { get; set; }
