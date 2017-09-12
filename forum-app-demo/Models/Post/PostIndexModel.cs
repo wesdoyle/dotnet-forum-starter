c1f1eededae8933bf511214d16446c88e26d4cba
Wes D <wesdoyle08@gmail.com>
2017-09-12 11:32:33 -0500

    Work on replies

---
+++ forum-app-demo/Models/Post/PostIndexModel.cs
@@ -0,0 +1,18 @@
+﻿using Forum.Web.Models.Reply;
+using System;
+using System.Collections.Generic;
+
+namespace Forum.Web.Models.Post
+{
+    public class PostIndexModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int AuthorRating { get; set; }
+        public DateTime Date { get; set; }
+        public string PostContent { get; set; }
+
+        public IEnumerable<PostReplyModel> Replies { get; set; }
+    }
+}
---
+++ forum-app-demo/Models/Reply/PostReplyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Forum.Web.Models.Reply
+{
+    public class PostReplyModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public int AuthorRating { get; set; }
+        public DateTime Date { get; set; }
+        public string ReplyContent { get; set; }
+    }
+}
