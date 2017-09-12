398dcd35c2d434620c347fa404abbb74bf646b41
Wes D <wesdoyle08@gmail.com>
2017-09-12 12:00:59 -0500

    Add profile controller

---
+++ forum-app-demo/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
