2083b351a320a8b5d0c1f8110b7e4707d9d0bd29
Wes D <wesdoyle08@gmail.com>
2017-09-12 12:02:02 -0500

    Add actions to profile controller

--- forum-app-demo/Controllers/ProfileController.cs
+++ forum-app-demo/Controllers/ProfileController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Forum.Data.Models;
 
 namespace Forum.Web.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
         {
             return View();
         }
