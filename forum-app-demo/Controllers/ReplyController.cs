24dd0825d6f1eb65561505803eb8737a9be4e58f
Wes D <wesdoyle08@gmail.com>
2017-09-12 13:50:59 -0500

    Work on replies

--- forum-app-demo/Controllers/ReplyController.cs
+++ forum-app-demo/Controllers/ReplyController.cs
@@ -3,6 +3,8 @@ using Forum.Data;
 using Forum.Web.Models.Reply;
 using Forum.Data.Models;
 using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
 
 namespace Forum.Web.Controllers
 {
@@ -21,20 +23,37 @@ namespace Forum.Web.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Post(int id)
+        public async Task<IActionResult> Post(int id)
         {
             var post = _postService.GetById(id);
             var forum = _forumService.GetById(post.Forum.Id);
-
+            var replyingUser = GetReplyingUser();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                 
             var model = new PostReplyModel
             {
+                PostContent = post.Content,
+                PostTitle = post.Title,
+                PostId = id.ToString(),
+
                 ForumName = forum.Title,
                 ForumId = forum.Id,
                 ForumImageUrl = forum.ImageUrl,
-                AuthorName = User.Identity.Name
+
+                AuthorName = User.Identity.Name,
+                AuthorImageUrl = user.ProfileImageUrl,
+                AuthorId = user.Id,
+                AuthorRating = user.Rating,
+
+                Date = DateTime.Now
             };
 
             return View(model);
+        }
+
+        private async Task<ApplicationUser> GetReplyingUser()
+        {
+            return await _userManager.GetUserAsync(User);
         }
     }
 }
