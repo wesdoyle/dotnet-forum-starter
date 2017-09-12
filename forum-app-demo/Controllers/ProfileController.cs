6d7c8b3e271eada037fc34fa982a8ae3ca71e6e2
Wes D <wesdoyle08@gmail.com>
2017-09-12 12:15:46 -0500

    Work on user profiles

--- forum-app-demo/Controllers/ProfileController.cs
+++ forum-app-demo/Controllers/ProfileController.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Forum.Data.Models;
+using Forum.Web.Models.ApplicationUser;
+using Forum.Data;
 
 namespace Forum.Web.Controllers
 {
     public class ProfileController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private IApplicationUser _userService;
 
-        public ProfileController(UserManager<ApplicationUser> userManager)
+        public ProfileController(UserManager<ApplicationUser> userManager, IApplicationUser userService)
         {
             _userManager = userManager;
+            _userService = userService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
-            return View();
+            var user = _userService.GetById(id);
+
+            var model = new ProfileModel()
+            {
+                UserId = user.Id,
+                Username = user.UserName,
+                Description = user.UserDescription,
+                Email = user.Email,
+                ProfileImageUrl = user.ProfileImageUrl
+            };
+
+            return View(model);
         }
 
         public IActionResult Edit()
