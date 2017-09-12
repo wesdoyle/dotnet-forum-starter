3a5c5a0df3c66f35e10f618d71b3e73df25b1c86
Wes D <wesdoyle08@gmail.com>
2017-09-12 13:41:33 -0500

    Work on replies

--- forum-app-demo/Models/Reply/PostReplyModel.cs
+++ forum-app-demo/Models/Reply/PostReplyModel.cs
@@ -5,12 +5,19 @@ namespace Forum.Web.Models.Reply
     public class PostReplyModel
     {
         public int Id { get; set; }
+
+        public string AuthorId { get; set; }
         public string AuthorName { get; set; }
         public int AuthorRating { get; set; }
-        public string AuthorId { get; set; }
         public string AuthorImageUrl { get; set; }
+
         public DateTime Date { get; set; }
         public string ReplyContent { get; set; }
+
+        public string PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+
         public string ForumName { get; set; }
         public string ForumImageUrl { get; set; }
         public int ForumId { get; set; }
