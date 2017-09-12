3f4bbbf0bf09e3fc9548ddc5e13fdd6a3ded5861
Wes D <wesdoyle08@gmail.com>
2017-09-12 12:03:15 -0500

    Add user profile model

---
+++ forum-app-demo/Models/ApplicationUser/ProfileModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Forum.Web.Models.ApplicationUser
+{
+    public class ProfileModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
