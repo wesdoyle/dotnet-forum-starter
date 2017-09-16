dca8eaee2cbbf938ee607942cd40fac4990c0759
Wes D <wesdoyle08@gmail.com>
2017-09-16 11:24:37 -0500

    Search model

---
+++ forum-app-demo/Models/Search/SearchResultModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Forum.Web.Models.Post;
+
+namespace Forum.Web.Models.Search
+{
+    public class SearchResultModel
+    {
+        public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
+        public bool EmptySearchResults { get; set; }
+    }
+}
